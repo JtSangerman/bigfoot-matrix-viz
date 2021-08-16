d8f1be3ba209d094dd62fc002468e2d720878b25
Jamie <jtsangerman@email.arizona.edu>
2021-08-15 17:33:30 -0700

    Initial commit. Console viz's with nearly the same board code. Next abstract and interface.

---
+++ src/BIGFOOT.RGBMatrix.LEDBoard.DriverInterfacing/Color.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BIGFOOT.RGBMatrix.LEDBoard.DriverInterfacing
+{
+    public class Color
+    {
+        public byte R;
+        public byte G;
+        public byte B;
+        public Color(int r, int g, int b)
+        {
+            R = (byte)r;
+            G = (byte)g;
+            B = (byte)b;
+        }
+        public Color(byte r, byte g, byte b)
+        {
+            R = r;
+            G = g;
+            B = b;
+        }
+    }
+}
