440ef9028019e57c1e4ab1a21531c66f519ddff1
JtSangerman <jtsangerman@email.arizona.edu>
2022-03-21 23:38:17 -0700

    working build

--- src/BIGFOOT.RGBMatrix/HardwareSnakeStartup.cs
+++ src/BIGFOOT.RGBMatrix/HardwareSnakeStartup.cs
@@ -1,14 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using BIGFOOT.RGBMatrix.Visuals.Snake;
+using BluetoothXboxOneControllerBIGFOOT.RGBMatrix.Visuals.Inputs;
+using rpi_rgb_led_matrix_sharp;
 
 namespace BIGFOOT.RGBMatrix
 {
-    public static class HardwareSnakeStartup
+    public class HardwareSnakeStartup
     {
-        public static void main(string[] args)
+        public static void Main(string[] args)
         {
-
+            var matrix = new RGBLedMatrix(32, 1, 1);
+            var controller = new BTXboxOneControllerDriver();
+            var vis = new Snake(matrix, controller);
+            vis.Run();
         }
     }
 }
